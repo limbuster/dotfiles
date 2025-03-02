cd92f7d62a822198dff38e897978a35f4f9d3260
Kumar Limbu <klimbu@finverse.com>
2025-03-02 17:38:38 +0800

    Show hidden files as well

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -7,7 +7,17 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
+    -- configure to use ripgrep
+    vimgrep_arguments = {
+      "rg",
+      "--follow",        -- Follow symbolic links
+      "--hidden",        -- Search for hidden files
+      "--no-heading",    -- Don't group matches by each file
+      "--with-filename", -- Print the file path with the matched lines
+      "--line-number",   -- Show line numbers
+      "--column",        -- Show column numbers
+      "--smart-case",    -- Smart case search
+    },
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
@@ -19,6 +29,15 @@ telescope.setup {
         ["<C-j>"] = actions.cycle_history_prev,
         ["<Down>"] = actions.move_selection_next,
         ["<Up>"] = actions.move_selection_previous,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      hidden = true,find_command = {
+        "rg",
+        "--files",
+        "--hidden",
       },
     },
   },
